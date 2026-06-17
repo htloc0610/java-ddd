8a6ef2ce045200ce214cad93411d2e5220d2700d
22120186 <12a1huynhtanloc@gmail.com>
2026-06-17 22:37:53 +0700

    feat: initialize multi-module Maven project with DDD architecture and basic Spring Boot integration

---
+++ xxxx-controller/src/main/java/com/xxxx/ddd/controller/resource/HiController.java
@@ -0,0 +1,19 @@
+package com.xxxx.ddd.controller.resource;
+
+import com.xxxx.ddd.application.service.event.impl.EventAppServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("hello")
+public class HiController {
+    @Autowired
+    private EventAppServiceImpl eventAppServiceImpl;
+
+    @GetMapping("/hi")
+    public String hello() {
+        return eventAppServiceImpl.sayHi("abc");
+    }
+}
